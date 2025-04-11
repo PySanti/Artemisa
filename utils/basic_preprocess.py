d910f121cd99fd64920e68f136b67ae890917482
santiago <santiagodeandrade4@gmail.com>
2025-04-11 19:25:09 -0400

    Implementacion de seleccion de caracteristicas

--- utils/basic_preprocess.py
+++ utils/basic_preprocess.py
@@ -4,6 +4,7 @@ from category_encoders import TargetEncoder
 from preprocess.encoding import CustomOneHotEncoding
 from preprocess.date_converter import DateConverter
 from preprocess.features_selection import BestFeatures
+from preprocess.scaler import CustomScaler
 import pandas as pd
 
 def basic_preprocess(df, target):
@@ -11,12 +12,12 @@ def basic_preprocess(df, target):
     target_encoding_cols    = ['location', 'agency', 'agent']
     oh_encoding_cols        = ['property_type', 'city', 'province_name', 'purpose']
     date_encoding           = ['date_added']
-    num_cols                = df.select_dtypes(exclude="object").columns.tolist()
 
     df = df.drop(dropable_cols, axis=1)
     df = df.drop_duplicates()
     df = df.drop(df.query("agency != agency and agent != agent").index)
     df = df.drop(155597)
+    num_cols = df.drop(target,axis=1).select_dtypes(exclude="object").columns.tolist()
 
     [df_train, df_test] = train_test_split(df, test_size=0.2, random_state=42, shuffle=True)
     df_test = df_test._append(df_train.iloc[0])
@@ -25,7 +26,8 @@ def basic_preprocess(df, target):
         ("target_encoding", TargetEncoder(verbose=10,cols=target_encoding_cols)),
         ("oh_encoding", CustomOneHotEncoding(attributes=oh_encoding_cols)),
         ("date_converter", DateConverter(date_encoding)),
-        ("features_selection", BestFeatures(0.02, False))
+        ("scaler", CustomScaler(num_cols)),
+        ("features_selection", BestFeatures(0.02, False)),
     ])
 
     print(df_train.iloc[0].to_frame().T)
@@ -39,6 +41,10 @@ def basic_preprocess(df, target):
 
     print(df_train.iloc[0].to_frame().T)
     print(df_test.iloc[-1].to_frame().T)
+
+
+    print(df_train.head(5))
+    print(df_test.head(5))
 
 
     return [df_train, df_test]
